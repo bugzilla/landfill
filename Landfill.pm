d35f047def64ca4ed514fb10b468a9ef96de5bbc
Max Kanat-Alexander <mkanat@bugzilla.org>
2010-11-08 14:35:27 -0800

    Add MPL boilerplate to all the Perl code.

--- Landfill.pm
+++ Landfill.pm
@@ -10,7 +10,7 @@
 # implied. See the License for the specific language governing
 # rights and limitations under the License.
 #
-# The Original Code is the Bugzilla Bug Tracking System.
+# The Original Code is the Landfill Tools System.
 #
 # The Initial Developer of the Original Code is Everything Solved, Inc.
 # Portions created by the Initial Developers are Copyright (C) 2010 the
